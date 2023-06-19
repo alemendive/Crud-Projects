3618f96677fdeaea2619d9ed5429160f21ad03e7
ottognr <alemendive@gmail.com>
2023-06-19 19:19:33 -0300

    salir boton

--- PeluqueriaCanina/src/main/java/com/alemendive/peluqueriacanina/igu/Principal.java
+++ PeluqueriaCanina/src/main/java/com/alemendive/peluqueriacanina/igu/Principal.java
@@ -38,6 +38,11 @@ public class Principal extends javax.swing.JFrame {
 
         jButton2.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         jButton2.setText("Salir");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         btnCargaDatos.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         btnCargaDatos.setText("Cargar Datos");
@@ -133,6 +138,10 @@ public class Principal extends javax.swing.JFrame {
        pantalla.setVisible(true);
        pantalla.setLocationRelativeTo(null);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
   
     // Variables declaration - do not modify//GEN-BEGIN:variables
